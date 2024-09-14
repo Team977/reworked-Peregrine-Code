e502d588cd80e52021da1ad968dbe3d0a5e16e84
billnye01 <105652816+billnye01@users.noreply.github.com>
2024-09-13 23:02:34 -0400

    add commands Organzition

--- src/main/java/frc/robot/subsystems/IO/IOJoystick.java
+++ src/main/java/frc/robot/subsystems/IO/IOJoystick.java
@@ -12,74 +12,74 @@ public class IOJoystick implements IOMoudlue {
     private static final CommandJoystick driverCon = new CommandJoystick(0);
     private static final CommandXboxController opporatorCon = new CommandXboxController(1);
 
-     public default DoubleSupplier getXPower() {
+     public  DoubleSupplier getXPower() {
     return () -> driverCon.getX();
   }
 
-  public default DoubleSupplier getYPower() {
+  public  DoubleSupplier getYPower() {
     return () -> driverCon.getY();
   }
 
-  public default DoubleSupplier getOmegaPower() {
+  public  DoubleSupplier getOmegaPower() {
     return () -> driverCon.getZ();
   }
 
-  public default Trigger getShooterReady() {
+  public  Trigger getShooterReady() {
     return opporatorCon.leftTrigger();
   }
 
-  public default Trigger getShoot() {
+  public  Trigger getShoot() {
     return driverCon.button(0);
   }
 
-  public default Trigger ReverseIntake() {
+  public  Trigger ReverseIntake() {
     return opporatorCon.b();
   }
 
-  public default Trigger Intake() {
+  public  Trigger Intake() {
     return opporatorCon.a();
   }
 
-  public default Trigger setModeSpeeker() {
+  public  Trigger setModeSpeeker() {
     return opporatorCon.y();
   }
 
-  public default Trigger setModeAmp() {
-    return opporatorCon.x();
+  public  Trigger setModeAmp() {
+    return opporatorCon.x();}
 
-  public default Trigger setModeNote() {
+  public  Trigger setModeNote() {
     return opporatorCon.rightBumper();
   }
 
-  public default Trigger setModeFeed() {
+  public  Trigger setModeFeed() {
     return opporatorCon.leftBumper();
   }
 
-  public default Trigger setAutoRotateOn() {
+  public  Trigger setAutoRotateOn() {
     return opporatorCon.back();
   }
 
-  public default Trigger setAutoRotateOff() {
+  public  Trigger setAutoRotateOff() {
     return opporatorCon.start();
   }
 
-  public default Trigger setPassiveSwitchOn() {
+  public  Trigger setPassiveSwitchOn() {
     return opporatorCon.leftStick();
   }
 
-  public default Trigger setPassiveSwitchOff() {
+  public  Trigger setPassiveSwitchOff() {
     return opporatorCon.rightBumper();
   }
 
-  public default Trigger setDriveModeFast() {
+  public  Trigger setDriveModeFast() {
     return driverCon.button(1);
   }
 
-  public default Trigger setDriveModeNormal() {
+  public  Trigger setDriveModeNormal() {
     return driverCon.button(2);
   }
 
-  public default Trigger setDriveModeSlow() {
+  public  Trigger setDriveModeSlow() {
     return driverCon.button(3);
   }
 
